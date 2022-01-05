3428eca6b637586064907e6dcbf12ac7e3df1a17
Kostiantyn Peschanov <constantine.peschanov@gmail.com>
2022-01-05 08:06:26 +0200

    setup returning client part from backend

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -7,5 +9,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    const stream = fs.createReadStream('./public/index.html', 'utf-8');
+
+    res.setHeader('content-type', 'text/html');
+
+    stream.pipe(res);
+});
 
 module.exports = app;
